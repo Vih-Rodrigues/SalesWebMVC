4f14bc36c18b0d33ddbd3befd50ceec7043d32ac
Vitoria Mercado <andrader.vitoria@gmail.com>
2024-11-03 14:40:47 -0300

    Seeding Service

--- SalesWebMvc/SalesWebMvc/Program.cs
+++ SalesWebMvc/SalesWebMvc/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
+using SalesWebMvc.Data;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<SalesWebMvcContext>(options =>
     options.UseMySql(
@@ -11,10 +12,21 @@ builder.Services.AddDbContext<SalesWebMvcContext>(options =>
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Add the SeedingService to the dependency injection system as a Scoped service, which means it will be created once per HTTP request.
+builder.Services.AddScoped<SeedingService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var seedingService = services.GetRequiredService<SeedingService>();
+        seedingService.Seed();
+    }
+}
+else // Configure the HTTP request pipeline.
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
